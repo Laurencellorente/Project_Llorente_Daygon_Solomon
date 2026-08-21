80211484c237e7ac09302da24c54fa369886f8bd
Laurencellorente <laurencellorente32@gmail.com>
2026-08-22 03:16:34 +0800

    Daygon_SystemReportViewModel/Index

---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/ViewModel/SystemReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_ELECTIVE_PREFINALS_PROJECT.ViewModels
+{
+    public class SystemReportViewModel
+    {
+        public string ReportTitle { get; set; } = "IT Operations Summary Report";
+        public DateTime GeneratedAt { get; set; } = DateTime.Now;
+        public int TotalTicketsProcessed { get; set; }
+        public int OpenTicketsCount { get; set; }
+        public int ResolvedTicketsCount { get; set; }
+        public int TotalAuditLogsRecorded { get; set; }
+    }
+}
