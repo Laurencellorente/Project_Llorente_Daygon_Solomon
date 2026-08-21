5041ef099752084a66f089a9f8e4f307eb089582
Lawrenceangelo-rgb <lawrenceangelosalomon98@gmail.com>
2026-08-22 02:03:20 +0800

    ticket6

---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/Models/TicketHistory.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Models
+{
+    [Table("TicketHistories")]
+    public class TicketHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TicketId { get; set; }
+
+        [ForeignKey("TicketId")]
+        public Ticket? Ticket { get; set; }
+
+        [Required]
+        public string Action { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ChangedAt { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+}
