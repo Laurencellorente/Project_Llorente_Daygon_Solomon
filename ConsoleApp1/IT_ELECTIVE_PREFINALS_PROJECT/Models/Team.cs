f1250280567c0ce5da5de0a0ccc08c812146837e
Laurencellorente <laurencellorente32@gmail.com>
2026-08-22 02:14:15 +0800

    Daygon_team/ticketCategory

---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/Models/Team.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        
+        [ForeignKey("DepartmentId")]
+        public Department? Department { get; set; }
+    }
+}
