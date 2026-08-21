466c1f9262684d10331eec509942d25ed0647262
Laurencellorente <laurencellorente32@gmail.com>
2026-08-21 22:04:43 +0800

    Implement IItemRepository and ItemRepository with DI registration

---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/Repositories/IItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using IT_ELECTIVE_PREFINALS_PROJECT.Models;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Repositories
+{
+    public interface IItemRepository : IRepository<Item>
+    {
+        
+        IEnumerable<Item> SearchByName(string name);
+    }
+}
---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_ELECTIVE_PREFINALS_PROJECT.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
