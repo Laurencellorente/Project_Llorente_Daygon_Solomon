f0679d38d40f9fa524fabc3a6c0259600d297e08
Laurencellorente <laurencellorente32@gmail.com>
2026-08-22 02:28:59 +0800

    Daygon_TicketComments/TicketDetailsViewModels

---
+++ ConsoleApp1/IT_ELECTIVE_PREFINALS_PROJECT/Models/TicketDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using IT_ELECTIVE_PREFINALS_PROJECT.Models;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.ViewModels
+{
+    public class TicketDetailsViewModel
+    {
+        public required Ticket Ticket { get; set; }
+        public IEnumerable<TicketComment> Comments { get; set; } = new List<TicketComment>();
+
+        
+        public string NewCommentText { get; set; } = string.Empty;
+        public bool IsInternal { get; set; } = false;
+    }
+}
